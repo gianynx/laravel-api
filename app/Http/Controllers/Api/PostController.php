38f9dee2bfc68463a8f8d2fc81193f49cfe3041b
Gianmarco Incocciati <minusgian@gmail.com>
2023-06-15 20:03:14 +0200

    modify PostController.php

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,16 +5,22 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Technology;
 
 class PostController extends Controller
 {
     public function index()
     {
         $posts = Post::with('technology', 'collaborators')->paginate(2);
+        $technologies = Technology::all();
+        $data = [
+            'posts' => $posts,
+            'technologies' => $technologies
+        ];
         return response()->json([
             'status' => 'success',
             'message' => 'Ok',
-            'results' => $posts
+            'results' => $data
         ], 200);
     }
 
